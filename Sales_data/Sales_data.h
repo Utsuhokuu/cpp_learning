6a7113147027bb19cc9c8b84441d02102ff2213e
Fowdef <fowdef@hotmail.com>
2021-05-03 20:53:34 +0800

    Store my coding practice

---
+++ Sales_data/Sales_data.h
@@ -0,0 +1,75 @@
+#ifndef SALES_DATA_H 
+#define SALES_DATA_H
+#include <string>
+#include <iostream>
+
+class Sales_data {
+//为Sales_data的非成员函数所做的友元声明	
+friend Sales_data add(const Sales_data&, const Sales_data&);
+friend std::istream &read(std::istream&, Sales_data&);
+friend std::ostream &print(std::ostream&, const Sales_data&);
+
+public:	
+	Sales_data() = dafault;//默认构造函数 
+	//构造函数初始化
+	Sales_data(const std::string &s): 
+			   bookNo(s), units_sold(0), revenue(0) { }
+	Sales_data(const std::string &s, unsigned cnt, double rev):
+			   bookNo(s), units_solds(cnt), revenue(rev*cnt){ }
+	// 调用read函数初始化构造函数 
+	Sales_data(std::istream &);
+		
+	std::string isbn() const { return bookNo; }
+	Sales_data& combine(const Sales_data&);
+	
+private:	
+	double avg_price() const
+		{ return units_sold ? revenue/units_sold : 0; }
+	std::string bookNo;
+	unsigned units_sold = 0;
+	double revenue = 0.0;
+};
+
+// Sales_data的非成员接口 
+Sales_data add(const Sales_data&, const Sales_data&);
+std::ostream &print(std::ostream&, const Sales_data&);
+std::istream &read(std::istream&, Sales_data&);
+
+inline
+Sales_data& Sales_data::combine(const Sales_data &rhs) {
+	units_sold += rhs.units_sold; // 把rhs的成员加到this对象的成员上
+	revenue += rhs.revenue;
+	return *this;
+} 
+
+// read函数：从is中读取一条交易信息然后存入item对象当中 
+inline
+istream &read(istream &is, Sales_data &item) {
+	double price = 0;
+	is >> item.bookNo >> item.units_sold >> price;
+	item.revenue = price * item.units_sold;
+	return is;
+}
+
+// print函数：将item中的信息通过os打印出来 
+inline
+ostream &print(ostream &os, const Sales_data &item) {
+	os << item.isbn() << " " << item.units_sold << " "
+	   << item.revenue << " " << item.avg_price();
+	return os;
+}
+
+// add函数：接收lhs与rhs两个Sales_data对象，并且返回该两个对象的和 
+Sales_data add(const Sales_data &lhs, const Sales_data &rhs) {
+	Sales_data sum = lhs;
+	sum.combine(rhs);
+	return sum;
+}// 由于返回local data，故返回类型是value而不是reference
+  
+Sales_data::Sales_data(std::istream &is) {
+	read(is, *this);
+}
+ 
+ 
+
+#endif
---
+++ complex/complex.h
@@ -0,0 +1,62 @@
+#ifndef __MY_COMPLEX_H__
+#define __MY_COMPLEX_H__
+
+class complex
+{
+public:
+	complex (double r = 0, double i = 0):  re (r), im (i) { }
+	complex& operator += (const complex&);
+	complex& operator -= (const complex&);
+ 	complex& operator *= (const complex&);
+	complex& operator /= (const complex&);
+	double real () const { return re; }
+	double imag () const { return im; }
+
+private:
+	double re, im;
+	
+	friend complex& __doapl (complex *, const complex&);
+};
+
+inline complex&
+__doapl(complex* ths, const complex& r)
+{
+	ths->re += r.re;
+	ths->im += r.im;
+	return *this
+}
+
+inline complex& 
+complex::operator += (const complex& r)
+{
+	return __doapl (this, r);
+}
+
+//把+操作符定义在类外，使得类可以应付复数加实数的情况，对- * /,同理 
+inline complex
+operator + (const complex& x, const complex& y)
+{
+	return complex (real (x) + real (y), imag (x) + imag (y));//local object
+}
+
+inline complex
+operator + (const complex& x, double y)
+{
+	return complex (real (x) + y, imag (x));
+}
+
+inline complex
+operator + (double x, const complex& y)
+{
+	return complex (x + real (y), imag (y));
+}
+
+#include <iostream>
+ostream&
+operator << (ostream& os, const complex& x)
+{
+	return os << '(' << real (x) << ',' << imag (x) << ')';
+} 
+
+
+#endif
