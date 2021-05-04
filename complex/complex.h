bd2f717de572011b2e103ffc34b5d78f2e42217a
Fowdef <fowdef@hotmail.com>
2021-05-04 14:13:23 +0800

    Complex类及其测试代码

--- complex/complex.h
+++ complex/complex.h
@@ -4,7 +4,8 @@
 class complex
 {
 public:
-	complex (double r = 0, double i = 0):  re (r), im (i) { }
+	// 默认构造函数,非指针类不需要考虑Big Three 
+	complex (double r = 0, double i = 0):  re (r), im (i) { } 
 	complex& operator += (const complex&);
 	complex& operator -= (const complex&);
  	complex& operator *= (const complex&);
@@ -16,6 +17,9 @@ private:
 	double re, im;
 	
 	friend complex& __doapl (complex *, const complex&);
+	friend complex& __doami (complex *, const complex&);
+	friend complex& __doaml (complex *, const complex&);
+	friend complex& __doadi (complex *, const complex&);
 };
 
 inline complex&
@@ -23,7 +27,7 @@ __doapl(complex* ths, const complex& r)
 {
 	ths->re += r.re;
 	ths->im += r.im;
-	return *ths
+	return *ths;
 }
 
 inline complex& 
@@ -37,7 +41,7 @@ __doami(complex* ths, const complex& r)
 {
 	ths->re -= r.re;
 	ths->im -= r.im;
-	return *ths
+	return *ths;
 }
 
 inline complex& 
@@ -46,23 +50,13 @@ complex::operator -= (const complex& r)
 	return __doami (this, r);
 }
 
-
-
 inline complex&
 __doaml(complex* ths, const complex& r)
 {
-	ths->re *= r.re;
-	ths->im *= r.im;
-	return *ths
-}
-
-inline complex&
-__doaml(complex* ths, const complex& r)
-{
-	double f = 0.0;
-	f = ths->re + r.re;
-	ths->im -= r.im;
-	return *ths
+	double f = ths->re * r.re - ths->im * r.im;
+	ths->im = ths->re * r.im + ths->im * r.re;
+	ths->re = f; 
+	return *ths;
 }
 
 inline complex& 
@@ -71,8 +65,32 @@ complex::operator *= (const complex& r)
 	return __doaml (this, r);
 }
 
+inline complex&
+__doadi(complex* ths, const complex& r)
+{
+	double g = (ths->im * r.re - ths->re * r.im) / (r.re * r.re + r.im * r.im);
+	ths->re = (ths->re * r.re + ths->im * r.im) / (r.re * r.re + r.im * r.im);
+	ths->im = g; 
+	return *ths;
+}
 
+inline complex& 
+complex::operator /= (const complex& r)
+{
+	return __doadi (this, r);
+}
 
+inline double
+imag (const complex& x)
+{
+	return x.imag ();
+}
+
+inline double
+real (const complex& x)
+{
+	return x.real ();
+}
 
 //把+操作符定义在类外，使得类可以应付复数加实数的情况，对- * /,同理 
 inline complex
@@ -93,12 +111,129 @@ operator + (double x, const complex& y)
 	return complex (x + real (y), imag (y));
 }
 
-#include <iostream>
-ostream&
-operator << (ostream& os, const complex& x)
+inline complex
+operator + (const complex& x)
 {
-	return os << '(' << real (x) << ',' << imag (x) << ')';
-} 
+	return x;
+}
 
+inline complex
+operator - (const complex& x, const complex& y)
+{
+	return complex (real (x) - real (y), imag (x) - imag (y));
+}
 
-#endif
+inline complex
+operator - (const complex& x, double y)
+{
+	return complex (real (x) - y, imag (x));
+}
+
+inline complex
+operator - (double x, const complex& y)
+{
+	return complex (x - real (y), - imag (y));
+}
+
+inline complex
+operator - (const complex& x)
+{
+	return complex (-real (x), -imag (x));
+}
+
+inline complex
+operator * (const complex& x, const complex& y)
+{
+	return complex (real (x) * real (y) - imag (x) * imag (y),
+				 real (x) * imag (y) + imag (x) * real (y));
+}
+
+inline complex
+operator * (const complex& x, double y)
+{
+	return complex (real (x) * y, imag (x) * y);
+}
+
+inline complex
+operator * (double x, const complex& y)
+{
+	return complex (x * real (y), x * imag (y));
+}
+
+inline complex
+operator / (const complex& x, const complex& y)
+{
+	return complex ((real (x) * real (y) + imag (x) * imag (y)) / (real (y) * real (y) + imag (y) * imag(y)),
+				 (imag (x) * real (y) - real (x) * imag (y)) / (imag (x) * imag (x) + imag (y) * imag(y)));
+}
+
+inline complex
+operator / (const complex& x, double y)
+{
+	return complex (real (x) / y, imag (x) / y);
+}
+
+inline complex
+operator / (double x, const complex& y)
+{
+	return complex ((x * real (y)) / (real (y) * real (y) + imag (y) * imag(y)),
+				 (-(x * imag (y)) / (real (y) * real (y) + imag (y) * imag(y))));
+}
+
+inline bool
+operator == (const complex& x, const complex& y)
+{
+	return real(x) == real (y) && imag (x) == imag (y); 
+}
+
+inline bool
+operator == (const complex& x, double y)
+{
+	return real(x) == y && imag (x) == 0; 
+}
+
+inline bool
+operator == (double x, const complex& y)
+{
+	return x == real (y) && imag (y) == 0; 
+}
+
+inline bool
+operator != (const complex& x, const complex& y)
+{
+	return real(x) != real (y) || imag (x) != imag (y); 
+}
+
+inline bool
+operator != (const complex& x, double y)
+{
+	return real(x) != y || imag (x) != 0; 
+}
+
+inline bool
+operator != (double x, const complex& y)
+{
+	return x != real (y) || imag (y) != 0; 
+}
+
+#include <cmath>
+
+inline complex
+polar (double r, double t)
+{
+	return complex (r * cos (t), r * sin (t));
+}
+
+inline complex
+conj (const complex& x)
+{
+	return complex (real (x), -imag (x));
+}
+
+inline double
+norm (const complex& x)
+{
+	return real (x) * real (x) + imag (x) * imag (x);
+}
+
+#endif  //__MY_COMPLEX_H__
---
+++ complex/complex_test.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include "complex.h"
+
+using namespace std;
+
+ostream&
+operator << (ostream& os, const complex& x)
+{
+	return os << '(' << real (x) << ',' << imag (x) << ')';
+}
+
+int main() {
+	complex c1 (2, 1);
+	complex c2 (4, 2);
+	complex c3 (c1);
+	complex c4 = c2; 
+	
+	cout << c1 << endl;
+	cout << c2 << endl;
+	cout << c3 << endl;
+	cout << c4 << endl;
+	cout << c1.imag() << endl;
+	cout << real(c2) << endl;
+	
+	cout << c1 + c2 << endl;
+	cout << c1 - c2 << endl;
+	cout << c1 * c2 << endl;
+	cout << c1 / c2 << endl;
+	
+	cout << c1 + 2 << endl;
+	cout << 2 - c1 << endl;
+	cout << c1 * 2 << endl;
+	cout << c1 / 2 << endl;
+	
+	cout << +c1 << endl;
+	cout << -c2 << endl;
+	
+	cout << (c1 += c2) << endl;
+	cout << (c1 -= c2) << endl;
+	cout << (c1 *= c2) << endl;
+	cout << (c1 /= c2) << endl;
+	
+	cout << (c1 == c2) << endl;
+	cout << (c1 != c2) << endl;
+	
+	cout << polar(6, 3) << endl;
+	cout << conj(c1) << endl;
+	cout << norm(c1) << endl;
+	
+	return 0;
+}
